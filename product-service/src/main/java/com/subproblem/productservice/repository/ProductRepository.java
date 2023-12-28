132aee34cffce64ccb6a29d8e0e75c15e6ded708
SubProblem <gukag6@gmail.com>
2023-12-29 02:34:38 +0400

    implementing kafka in product-service

--- product-service/src/main/java/com/subproblem/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/subproblem/productservice/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.subproblem.productservice.repository;
 import com.subproblem.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findProductById(Integer id);
 }
